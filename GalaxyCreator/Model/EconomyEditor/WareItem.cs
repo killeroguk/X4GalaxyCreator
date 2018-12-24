ed4032222ba7b1d1856592dd05957570557b6433
Kenny De Smedt <de_smedt_kenny@telenet.be>
2018-12-24 12:39:06 +0100

    Detail dialog for the economy editor

---
+++ GalaxyCreator/Model/EconomyEditor/WareItem.cs
@@ -0,0 +1,30 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+
+namespace GalaxyCreator.Model.EconomyEditor
+{
+    public class WareItem : ObservableObject
+    {
+        private String _value = default(String);
+
+        public WareItem() { }
+
+        public WareItem(string ware)
+        {
+            this._value = ware;
+        }
+
+        public String Value
+        {
+            get { return _value; }
+            set
+            {
+                if (value != _value)
+                {
+                    _value = value;
+                    Set(ref _value, value);
+                }
+            }
+        }
+    }
+}
